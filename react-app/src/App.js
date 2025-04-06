e260eb6b309a1d8d0f0ddc814704c34a61ad811a
CraigGent <genty83@outlook.com>
2025-04-06 15:14:18 +0100

    Added react boilerplate and removed unwanted files

---
+++ react-app/src/App.js
@@ -0,0 +1,10 @@
+import Titlebar from "./components/titlebar";
+
+function App() {
+  return (
+    <div className="app-container app-layout">
+      <Titlebar />
+    </div>
+  );
+}
+export default App;
---
+++ react-app/src/components/titlebar.js
@@ -0,0 +1,16 @@
+const titlebar = () => {
+  return (
+    <div className="titlebar">
+      <div>
+        <h1>This is my titlebar</h1>
+      </div>
+      <div>
+        
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+export default titlebar;
