a32cb54c04f8bed5c415c305a722f00eb42648b9
CraigGent <genty83@outlook.com>
2025-04-07 20:16:35 +0100

    Setup routing

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,18 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Titlebar from "./components/titlebar/titlebar";
 import Sidebar from "./components/sidebar/sidebar";
 import Bottombar from "./components/bottombar/bottombar";
 import NavMenu from "./components/nav-menu/nav-menu";
 import Breadcrumb from "./components/breadcrumb/breadcrumb";
+import Home from "./pages/home"
 
 function App() {
   return (
-    <div className="app-container app-layout">
-      <Titlebar />
-      <Sidebar />
-      <NavMenu />
-      <Breadcrumb />
-      <Bottombar />
-    </div>
-  );
+    <Router>
+      <div className="app-container app-layout">
+        <Titlebar />
+        <Sidebar />
+        <NavMenu />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* Add more routes here as needed */}
+          </Routes>
+        </div>
+        <Breadcrumb />
+        <Bottombar />
+      </div>
+    </Router>
+    );
 }
 export default App;
---
+++ react-app/src/pages/settings.js
@@ -0,0 +1,10 @@
+const Settings = () => {
+  return (
+    <div>
+      <h1>Settings Page</h1>
+      <p>Configure your app settings here.</p>
+    </div>
+  );
+};
+
+export default Settings;
